bf88cff7ebe66db2f9a7cf704db29f87d3b047c5
lucarlc <luca.relic@web.de>
2026-01-18 10:03:46 +0100

    fehlende imports

--- src/main/java/htw/webtech/WT_todo/persistence/repository/TodoRepository.java
+++ src/main/java/htw/webtech/WT_todo/persistence/repository/TodoRepository.java
@@ -3,6 +3,8 @@ package htw.webtech.WT_todo.persistence.repository;
 import htw.webtech.WT_todo.persistence.entity.TodoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 /**
  * TodoRepository ist die Schnittstelle zur Datenbank.
