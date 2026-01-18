16a10b6b9464afce411dea52eae777b9e6ba3443
lucarlc <luca.relic@web.de>
2026-01-18 19:53:40 +0100

    backend testing anpassungen

--- src/test/java/htw/webtech/WT_todo/TodoRepositoryTest.java
+++ src/test/java/htw/webtech/WT_todo/TodoRepositoryTest.java
@@ -1,6 +1,7 @@
 package htw.webtech.WT_todo;
 
 import htw.webtech.WT_todo.persistence.entity.TodoEntity;
+import htw.webtech.WT_todo.persistence.entity.UserEntity;
 import htw.webtech.WT_todo.persistence.repository.TodoRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,22 +17,40 @@ class TodoRepositoryTest {
     @Autowired
     private TodoRepository todoRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Test
-    void shouldDeleteAllCompletedTodos() {
-        TodoEntity a = new TodoEntity();
-        a.setTitle("A");
-        a.setDone(true);
+    void shouldDeleteAllCompletedTodosForSpecificUserOnly() {
+        UserEntity userA = userRepository.save(new UserEntity("alice", "x"));
+        UserEntity userB = userRepository.save(new UserEntity("bob", "x"));
 
-        TodoEntity b = new TodoEntity();
-        b.setTitle("B");
-        b.setDone(false);
+        TodoEntity a1 = new TodoEntity();
+        a1.setTitle("A1");
+        a1.setDone(true);
+        a1.setUser(userA);
 
-        todoRepository.save(a);
-        todoRepository.save(b);
+        TodoEntity a2 = new TodoEntity();
+        a2.setTitle("A2");
+        a2.setDone(false);
+        a2.setUser(userA);
 
-        int deleted = todoRepository.deleteByDoneTrue();
+        TodoEntity b1 = new TodoEntity();
+        b1.setTitle("B1");
+        b1.setDone(true);
+        b1.setUser(userB);
+
+        todoRepository.save(a1);
+        todoRepository.save(a2);
+        todoRepository.save(b1);
+
+        int deleted = todoRepository.deleteByUserAndDoneTrue(userA);
+
         assertThat(deleted).isEqualTo(1);
-        assertThat(todoRepository.findAll()).hasSize(1);
-        assertThat(todoRepository.findAll().get(0).isDone()).isFalse();
+        assertThat(todoRepository.findByUser(userA)).hasSize(1);
+        assertThat(todoRepository.findByUser(userA).get(0).getTitle()).isEqualTo("A2");
+
+        assertThat(todoRepository.findByUser(userB)).hasSize(1);
+        assertThat(todoRepository.findByUser(userB).get(0).getTitle()).isEqualTo("B1");
     }
 }
